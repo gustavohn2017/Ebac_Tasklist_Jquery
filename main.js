48eaec37d67ed90991a3092dc6fcb908e4aaf351
Gustavo <gustavohn2017@gmail.com>
2024-05-27 18:06:43 -0300

    new commit

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-    var input = $(this.val())
-    $('ul').append('<li>'+ input + '<i class="fas fa-check"></i> </li>');
-    $(this).val('');
+    $('#input').change(function(){
+        var input = $(this).val();
+        $('ul').append('<li>'+ input + '<i class="fas fa-check"></i> <i class="fas fa-trash"></i> </li>');
+        $(this).val('');
+    });
+
+    $('ul').on('click', '.fa-check',function(){
+        $(this).parent('li').toggleClass('checked')
+    });
+
+    $('ul').on('click', '.fa-trash', function(){
+        $(this).parent('li').fadeOut(200);
+    });
+    
 });
