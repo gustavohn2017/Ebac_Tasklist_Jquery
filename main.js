f5b04e34cb7eeb2f7746bdd30e601d2d09447ffe
Gustavo <gustavohn2017@gmail.com>
2024-05-28 06:41:58 -0300

    Atualizando conteúdo da Task List

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
+    //Lê o input e armazena na lista
     $('#input').change(function(){
         var input = $(this).val();
         $('ul').append('<li>'+ input + '<i class="fas fa-check"></i> <i class="fas fa-trash"></i> </li>');
         $(this).val('');
     });
-
+    // ao clicar na checkmark, altera a css para "checked", adicionando line through "riscando" a tarefa.
     $('ul').on('click', '.fa-check',function(){
         $(this).parent('li').toggleClass('checked')
     });
-
+    // Literalmente apaga a tarefa com o fadeout.
     $('ul').on('click', '.fa-trash', function(){
         $(this).parent('li').fadeOut(200);
     });
